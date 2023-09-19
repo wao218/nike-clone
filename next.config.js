d929e11e8f797769afa11866453fe5d0553fdd14
wao218 <wesleyosborne1@gmail.com>
2023-09-19 16:40:04 -0700

    try again

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
-}
+  images: {
+    domains: ['https://nike-clone-lac-beta.vercel.app/'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
